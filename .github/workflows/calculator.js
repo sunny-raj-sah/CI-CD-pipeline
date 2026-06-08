a3e419adeaa10efda5b2dc50b7976d4449f4d7b0
Sunny raj <sunnyraj01000@gmail.com>
2026-06-08 20:42:43 +0530

    Create calculator.js

---
+++ .github/workflows/calculator.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    const neg = (~b + 1) | 0;
+    let sum = a;
+    let carry = neg;
+    while (carry) {
+        const next = sum ^ carry;
+        carry = (sum & carry) << 1;
+        sum = next;
+    }
+    return (sum - ((a & b) >>> 5)) | 0;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide
+};
